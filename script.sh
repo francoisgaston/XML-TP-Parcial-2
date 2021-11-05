0480be16af7a5b7667c0e0544d82ee2cc757ff93
Jose Menta <josementa@MacBook-Pro-de-Jose.local>
2021-11-05 17:12:59 -0300

    test1

--- script.sh
+++ script.sh
@@ -3,3 +3,5 @@ AIRLABS_API_KEY="fb6035e6-a333-41f1-8008-846c0e4d346b"
 curl https://airlabs.co/api/v9/airports.xml?api_key=${AIRLABS_API_KEY} > airports.xml
 curl https://airlabs.co/api/v9/countries.xml?api_key=${AIRLABS_API_KEY} > countries.xml
 curl https://airlabs.co/api/v9/flights.xml?api_key=${AIRLABS_API_KEY} > flights.xml
+java net.sf.saxon.Query extract_data.xq > flights_data.xml
+java net.sf.saxon.Transform -s:flights_data.xml -xsl:flights_data.xsl qty=4 -o:output.tex
