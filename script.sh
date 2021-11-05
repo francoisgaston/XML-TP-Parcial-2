b589a56e648a5156971662aa56af3bbbdb1efdc5
gfrancois <gfrancois@itba.edu.ar>
2021-11-05 19:45:25 -0300

    actualizacion previa a instalacion

--- script.sh
+++ script.sh
@@ -4,4 +4,9 @@ curl https://airlabs.co/api/v9/airports.xml?api_key=${AIRLABS_API_KEY} > airport
 curl https://airlabs.co/api/v9/countries.xml?api_key=${AIRLABS_API_KEY} > countries.xml
 curl https://airlabs.co/api/v9/flights.xml?api_key=${AIRLABS_API_KEY} > flights.xml
 java net.sf.saxon.Query extract_data.xq > flights_data.xml
-java net.sf.saxon.Transform -s:flights_data.xml -xsl:flights_data.xsl qty=4 -o:output.tex
+if [$1 = ""]
+then
+    java net.sf.saxon.Transform -s:flights_data.xml -xsl:flights_data.xsl -o:output.tex
+else
+    java net.sf.saxon.Transform -s:flights_data.xml -xsl:flights_data.xsl qty=$1 -o:output.tex
+fi
